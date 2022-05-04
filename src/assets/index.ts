c05195b2a843841c7c36227af60dc6d68d5c6fbb
Marcus Vinicius Coelho Guimarães Filho <marcusvnfilho@gmail.com>
2022-05-04 16:11:43 -0300

    feat: react native svg, new fonts and images

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import StarWarsLogo from './star-wars-logo.png';
+
+import IconFavorite from './favorite.svg';
+import IconFavoriteActive from './favorite-active.svg';
+
+const Images = {
+  StarWarsLogo,
+};
+
+const Icons = {
+  IconFavorite,
+  IconFavoriteActive,
+};
+
+export {Icons, Images};
